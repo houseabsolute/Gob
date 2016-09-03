f0ad4cc887358fc42dc7d454e055bbc2259530fe
Dave Rolsky <autarch@urth.org>
2016-09-03 14:09:46 -0500

    Update schema to separate out flow table and add foreign keys separately

--- schema/deploy/base-schema.sql
+++ schema/deploy/base-schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE "user" (
 
 CREATE TABLE resume (
     resume_id     UUID      PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id       UUID      NOT NULL REFERENCES "user" (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    user_id       UUID      NOT NULL,
     uri           web_uri   DEFAULT '',
     file          TEXT      DEFAULT '',
     created_at    TIMESTAMPTZ  DEFAULT now(),
@@ -32,20 +32,72 @@ CREATE TABLE posting (
     posting_id    UUID      PRIMARY KEY DEFAULT uuid_generate_v4(),
     title         citext    NOT NULL,
     description   TEXT      NOT NULL,
+    user_id       UUID      NOT NULL,
+    flow_id       UUID      NOT NULL,
+    created_at    TIMESTAMPTZ  DEFAULT now(),
+    last_updated_at  TIMESTAMPTZ  DEFAULT now()
+);
+
+CREATE TABLE flow (
+    flow_id       UUID      PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title         citext    NOT NULL,
     flow          JSONB     NOT NULL,
+    user_id       UUID      NOT NULL,
     created_at    TIMESTAMPTZ  DEFAULT now(),
     last_updated_at  TIMESTAMPTZ  DEFAULT now()
 );
 
 CREATE TABLE application (
-    user_id       UUID      NOT NULL REFERENCES "user" (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    posting_id    UUID      NOT NULL REFERENCES posting (posting_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    user_id       UUID      NOT NULL,
+    posting_id    UUID      NOT NULL,
     cover_letter  TEXT      NOT NULL,
     resume        UUID      NOT NULL,
     state         JSONB     NOT NULL,
     created_at    TIMESTAMPTZ  DEFAULT now(),
-    last_updated_at  TIMESTAMPTZ  DEFAULT now()
+    last_updated_at  TIMESTAMPTZ  DEFAULT now(),
+    PRIMARY KEY (user_id, posting_id)
 );
 
+ALTER TABLE resume
+    ADD CONSTRAINT resume_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES "user" (user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE posting
+    ADD CONSTRAINT posting_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES "user" (user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE posting
+    ADD CONSTRAINT posting_flow_id_fkey FOREIGN KEY (flow_id)
+    REFERENCES flow (flow_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE flow
+    ADD CONSTRAINT flow_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES "user" (user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE application
+    ADD CONSTRAINT application_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES "user" (user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE application
+    ADD CONSTRAINT application_posting_id_fkey FOREIGN KEY (posting_id)
+    REFERENCES posting (posting_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
 
 COMMIT;
