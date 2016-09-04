8c3f75a494aedc46723a9757a66fe9a68ea166cc
Dave Rolsky <autarch@urth.org>
2016-09-04 09:47:31 -0500

    Break out flow into its own tables

--- schema/deploy/base-schema.sql
+++ schema/deploy/base-schema.sql
@@ -41,21 +41,42 @@ CREATE TABLE posting (
 CREATE TABLE flow (
     flow_id       UUID      PRIMARY KEY DEFAULT uuid_generate_v4(),
     title         citext    NOT NULL,
-    flow          JSONB     NOT NULL,
     user_id       UUID      NOT NULL,
     created_at    TIMESTAMPTZ  DEFAULT now(),
     last_updated_at  TIMESTAMPTZ  DEFAULT now()
 );
 
+CREATE TYPE step_type AS ENUM ( 'Review', 'Questionnaire', 'Interview', 'Reference Checking' );
+
+CREATE TABLE flow_step (
+    flow_step_id  UUID      PRIMARY KEY DEFAULT uuid_generate_v4(),
+    flow_id       UUID      NOT NULL,
+    step_order    INT       NOT NULL,
+    step_type     step_type NOT NULL,
+    title         citext    NOT NULL,
+    body          JSONB     NOT NULL,
+    user_id       UUID      NOT NULL,
+    created_at    TIMESTAMPTZ  DEFAULT now(),
+    last_updated_at  TIMESTAMPTZ  DEFAULT now(),
+    UNIQUE (flow_id, step_order)
+);
+
 CREATE TABLE application (
+    application_id  UUID    PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id       UUID      NOT NULL,
     posting_id    UUID      NOT NULL,
     cover_letter  TEXT      NOT NULL,
     resume        UUID      NOT NULL,
-    state         JSONB     NOT NULL,
     created_at    TIMESTAMPTZ  DEFAULT now(),
     last_updated_at  TIMESTAMPTZ  DEFAULT now(),
-    PRIMARY KEY (user_id, posting_id)
+    UNIQUE (user_id, posting_id)
+);
+
+CREATE TABLE application_flow_step (
+    application_id  UUID    NOT NULL,
+    flow_step_id  UUID      NOT NULL,
+    step_state    JSONB     NOT NULL,
+    PRIMARY KEY (application_id, flow_step_id)
 );
 
 ALTER TABLE resume
@@ -86,6 +107,13 @@ ALTER TABLE flow
     ON UPDATE CASCADE
     DEFERRABLE;
 
+ALTER TABLE flow_step
+    ADD CONSTRAINT flow_step_flow_id_fkey FOREIGN KEY (flow_id)
+    REFERENCES flow (flow_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
 ALTER TABLE application
     ADD CONSTRAINT application_user_id_fkey FOREIGN KEY (user_id)
     REFERENCES "user" (user_id)
@@ -96,6 +124,20 @@ ALTER TABLE application
 ALTER TABLE application
     ADD CONSTRAINT application_posting_id_fkey FOREIGN KEY (posting_id)
     REFERENCES posting (posting_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE application_flow_step
+    ADD CONSTRAINT application_flow_step_application_id_fkey FOREIGN KEY (application_id)
+    REFERENCES application (application_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    DEFERRABLE;
+
+ALTER TABLE application_flow_step
+    ADD CONSTRAINT application_flow_step_flow_step_id_fkey FOREIGN KEY (flow_step_id)
+    REFERENCES flow_step (flow_step_id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
     DEFERRABLE;
--- schema/verify/base-schema.sql
+++ schema/verify/base-schema.sql
@@ -5,6 +5,9 @@ BEGIN;
 SELECT pg_catalog.has_table_privilege( 'user', 'select' );
 SELECT pg_catalog.has_table_privilege( 'resume', 'select' );
 SELECT pg_catalog.has_table_privilege( 'posting', 'select' );
+SELECT pg_catalog.has_table_privilege( 'flow', 'select' );
+SELECT pg_catalog.has_table_privilege( 'flow_step', 'select' );
 SELECT pg_catalog.has_table_privilege( 'application', 'select' );
+SELECT pg_catalog.has_table_privilege( 'application_flow_step', 'select' );
 
 ROLLBACK;
