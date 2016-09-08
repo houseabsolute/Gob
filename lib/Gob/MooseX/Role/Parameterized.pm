62d138879fd392da80d1849e749665f02004e079
Dave Rolsky <autarch@urth.org>
2016-09-08 17:13:50 -0500

    Add Gob wrapper for MX::Role::Parameterized

---
+++ lib/Gob/MooseX/Role/Parameterized.pm
@@ -0,0 +1,32 @@
+package Gob::MooseX::Role::Parameterized;
+
+use Import::Into;
+use Moose::Exporter;
+use MooseX::Role::Parameterized    ();
+use MooseX::SemiAffordanceAccessor ();
+use namespace::autoclean           ();
+
+use g;
+
+my ($import) = Moose::Exporter->setup_import_methods(
+    install => [ 'unimport', 'init_meta' ],
+    also    => ['Moose::Role'],
+);
+
+sub import ( $class, @ ) {
+    my $for_class = caller();
+
+    $import->( undef, { into => $for_class } );
+
+    MooseX::SemiAffordanceAccessor->import( { into => $for_class } );
+
+    # Note that we need to use a level here rather than a classname so that
+    # importing autodie works.
+    my $level = 1;
+    g->import::into($level);
+    namespace::autoclean->import::into($level);
+
+    return;
+}
+
+1;
